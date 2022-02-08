153895503ff5b2cb1682c9e2beb6550dddd21efd
dmac24 <dinitayala@gmail.com>
2022-02-08 02:23:37 +0000

    commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,6 +18,30 @@ class Rectangle:
         self.y = y
         super().__init__(id)
 
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
+
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
+
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
+
     @property
     def width(self):
         return self.__width
@@ -77,3 +101,6 @@ class Rectangle:
             raise ValueError("y must be >= 0")
 
         self.__y = value
+
+
+        
