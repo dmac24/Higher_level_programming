a280c5588e861b2daaacbe9458073d7be2c48ad5
dmac24 <dinitayala@gmail.com>
2022-02-08 03:04:18 +0000

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,3 +19,24 @@ class Base:
             self.id = self.__nb_objects
         else:
             self.id = id
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return []
+        return json.dumps(list_dictionaries)
+    @classmethod
+    def save_to_file(cls, list_objs):
+        listy = []
+        extra = len("_Rectangle__")
+        filename = cls.__name__ + ".json"
+        for objects in list_objs:
+            listy.append(objects.__dict__)
+        dicty = json.loads(json.dumps(listy))
+        for x in dicty:
+            for i in x:
+		if len(i) > extra:
+			i = i[extra:]
+                print("dicty --", i, x[i])
+
+        with open(filename, 'w') as f:
+            f.write(Base.to_json_string(listy))
