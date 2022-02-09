3dc170cb0a9bdfd4f5136833fa7fbf0fb5a4badd
Diana M. Ayala <56034849+dmac24@users.noreply.github.com>
2022-02-09 08:33:30 -0500

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""
-This module contains the "Rectangle" for base class
-"""
+"""This module contains the "Rectangle" for base class"""
 
 from models.base import Base
 
 
 class Rectangle(Base):
     """Defines a Rectangle"""
+    
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
+        
         self.width = width
         self.height = height
         self.x = x
