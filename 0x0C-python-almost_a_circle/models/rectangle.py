dec74cd9f0545fee945ab95fd5706694e4ae8ead
dmac24 <dinitayala@gmail.com>
2022-02-14 02:37:35 +0000

    style

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Defines a Rectangle""
+    """Defines a Rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
