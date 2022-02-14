8b655dfdb8ad9b273970d5400e0f128db502b1a1
Diana M. Ayala <56034849+dmac24@users.noreply.github.com>
2022-02-13 21:17:24 -0500

    pep8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,11 +5,11 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Defines a Rectangle"""
-    
+    """Defines a Rectangle""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        
+
         self.width = width
         self.height = height
         self.x = x
