c9c4a9265038a61552e456f436dae76d35e3fdc9
dmac24 <dinitayala@gmail.com>
2022-05-10 20:28:05 -0500

    tasks

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.'''
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    name = argv[4]
+    cur.execute("""SELECT * FROM states ORDER BY id ASC""".format(name))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == name:
+            print(row)
+    cur.close()
