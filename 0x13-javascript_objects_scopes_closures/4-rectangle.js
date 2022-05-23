1ff9764330cd0e7299928d830663384398ddba3e
dmac24 <dinitayala@gmail.com>
2022-05-23 10:31:11 +0000

    fix error

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i;
+    let j;
+    let line = '';
+    for (i = 0; i < this.width; i++) {
+      line += 'X';
+    }
+    for (j = 0; j < this.height; j++) {
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
